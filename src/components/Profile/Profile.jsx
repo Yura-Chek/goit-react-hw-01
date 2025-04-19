ad5ba5309651c495f7ecca20a3ffedff6faee963
Chekan <ky.chekan@gmail.com>
2025-04-19 20:36:17 +0300

    add css for task 1 + animation

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,25 +1,25 @@
 import css from "./Profile.module.css";
 
 const Profile = ({ name, tag, location, image, stats }) => (
-  <div>
-    <div>
-      <img src={image} alt="User avatar" />
-      <p>{name}</p>
-      <p>@{tag}</p>
-      <p>{location}</p>
+  <div className={css.profile}>
+    <div className={css.profileWrapper}>
+      <img className={css.profileImg} src={image} alt="User avatar" />
+      <p className={css.profileName}>{name}</p>
+      <p className={css.profileInf}>@{tag}</p>
+      <p className={css.profileInf}>{location}</p>
     </div>
-    <ul>
-      <li>
-        <span>Followers</span>
-        <span>{stats.followers}</span>
+    <ul className={css.profileList}>
+      <li className={css.profileItem}>
+        <span className={css.profileListInf}>Followers</span>
+        <span className={css.profileListInf}>{stats.followers}</span>
       </li>
-      <li>
-        <span>Views</span>
-        <span>{stats.views}</span>
+      <li className={css.profileItem}>
+        <span className={css.profileListInf}>Views</span>
+        <span className={css.profileListInf}>{stats.views}</span>
       </li>
-      <li>
-        <span>Likes</span>
-        <span>{stats.likes}</span>
+      <li className={css.profileItem}>
+        <span className={css.profileListInf}>Likes</span>
+        <span className={css.profileListInf}>{stats.likes}</span>
       </li>
     </ul>
   </div>
